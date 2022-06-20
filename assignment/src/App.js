7c2d0dd7285f94ae782dcc7e43f03867666006a4
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-06-20 16:20:44 +0530

    react app setup done

---
+++ assignment/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
